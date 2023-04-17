4a5d2390dbdacb86f47b84c5c57101d880023696
RubensJr21 <junior.correa2001@gmail.com>
2023-04-17 13:11:30 -0300

    Implementa algumas funções de criação e estruturas, e implementa alguns preenchimentos de lista

--- Trabalho 1 - FILOSOFOS/main.c
+++ Trabalho 1 - FILOSOFOS/main.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
+#include <math.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -47,17 +48,37 @@ ANOTAÇÕES:
 
 typedef int boolean;
 
+int floor_int(double num)
+{
+	return ((int)floor((double)num / 2));
+}
+
 typedef struct {
+	int id;
 	sem_t sem_pode_comer;
 } hashi_t;
+
+hashi_t* create_hashi(int id, sem_t sem)
+{
+	hashi_t* h = (hashi_t*)malloc(sizeof(hashi_t));
+	h->id = id;
+	h->sem_pode_comer = sem;
+	return h;
+}
 
 typedef struct {
 	int id;
 } filosofo_t;
 
+filosofo_t* create_filoso(int id)
+{
+	filosofo_t* f = (filosofo_t*)malloc(sizeof(filosofo_t));
+	f->id = id;
+	return f;
+}
+
 typedef struct {
 	boolean alguem_comeu;
-	//hread_cond_t condicao;
 	pthread_mutex_t mutex;
 	sem_t sem;
 } comeu_o_estipulado_t;
@@ -75,7 +96,7 @@ typedef struct {
 	comeu_o_estipulado_t* alguem_comeu_estipulado;
 } pkg_filosofo_t;
 
-pkg_filosofo_t* create_pkg_filosofo_t(filosofo_t* f, hashi_t* h1, hashi_t* h2, filosofo_t* lst_f, int* qtd_max_filosofos_comendo, int* qtd_que_precisa_comer, comeu_o_estipulado_t* comeram_o_estipulado) {
+pkg_filosofo_t* create_pkg_filosofo(filosofo_t* f, hashi_t* h1, hashi_t* h2, filosofo_t* lst_f, int* qtd_max_filosofos_comendo, int* qtd_que_precisa_comer, comeu_o_estipulado_t* comeram_o_estipulado) {
 	pkg_filosofo_t* pkg = (pkg_filosofo_t*)malloc(sizeof(pkg_filosofo_t));
 	pkg->filosofo = f;
 	pkg->hashi_1 = h1;
@@ -123,27 +144,51 @@ void* filosofo(void* arg)
 int main()
 {
 	int n_filosofos = 5, vezes_a_comer = 10000;
+	
+	const int qtd_max_filososfos_comendo = n_filosofos - floor_int(n_filosofos / 2.0);
 
-	sem_t sem_alguem_comeu;
-	sem_init(&sem_alguem_comeu, 0, 1);
-
-	/*pthread_cond_t cond;
-	pthread_cond_init(&cond, NULL);
-
-	pthread_cond_signal(cond);*/
+	sem_t sem_alguem_comeu_o_estipulado;
+	sem_init(&sem_alguem_comeu_o_estipulado, 0, 1); // Começa liberado para um acesso porque o primeiro que acessar já mudará a flag que todos irão ler
 
 	pthread_mutex_t mutex;
 	pthread_mutex_init(&mutex, NULL);
 
-	comeu_o_estipulado_t alguem;
-	alguem.alguem_comeu = FALSE;
-	//alguem.condicao = cond;
-	alguem.mutex = mutex;
-	alguem.sem = sem_alguem_comeu;
+	comeu_o_estipulado_t* estipulado = (comeu_o_estipulado_t *)malloc(sizeof(comeu_o_estipulado_t));
+	estipulado->alguem_comeu = FALSE;
+	//estipulado->condicao = cond;
+	estipulado->mutex = mutex;
+	estipulado->sem = sem_alguem_comeu_o_estipulado;
 	
 	// lista de thread de filosofos
+	filosofo_t** filosofos = (filosofo_t**)malloc(sizeof(filosofo_t*) * n_filosofos);
+	int i;
+	for (i = 0; i < n_filosofos; i++)
+	{
+		filosofos[i] = create_filoso(i);
+	}
+
+	// lista de semaforos para os hashis
+	sem_t* semaphores_hashis = (sem_t*)mallolc(sizeof(sem_t) * n_filosofos);
+	for (i = 0; i < n_filosofos; i++)
+	{
+		sem_init(semaphores_hashis[i], 0, 1);
+	}
 
 	// lista de hashis
+	hashi_t** hashis = (hashi_t**)malloc(sizeof(hashi_t) * n_filosofos);
+	for (i = 0; i < n_filosofos; i++)
+	{
+		hashis[i] = create_hashi(i, semaphores_hashis[i]);
+	}
+
+	// lista de pkgs_filosofos
+	pkg_filosofo_t** pkgs = (pkg_filosofo_t*)malloc(sizeof(pkg_filosofo_t) * n_filosofos);
+	for (i = 0; i < n_filosofos; i++)
+	{
+		pkgs[i] = create_pkg_filosofo(filosofos[i], hashis[i], hashis[i + 1], filosofos, &qtd_max_filososfos_comendo, &vezes_a_comer, &estipulado);
+	}
+
+	// criação das threads
 
 	return 0;
 }
