10b9bdde8858811cac232e2bdf689a7c628232a6
RubensJr21 <junior.correa2001@gmail.com>
2023-05-06 11:52:00 -0300

    Tenta corrigir erro do malloc

--- Lista 5/main.c
+++ Lista 5/main.c
@@ -15,7 +15,7 @@
 	Condições:
 		-> precisa ter 20 mensagens para enviar
 	Estados:
-		-> o pombo mora (começa) no site_A
+		-> o pombo "mora"/"começa" no site_A
 		-> o pombo dorme até que tenha o número de mensagens (20) para enviar
 		-> quando voltar ele dorme até ter o número de mensagens (20) para enviar
 	Regras:
@@ -39,68 +39,133 @@
 #include <stdlib.h>
 #include <Windows.h>
 
+#define TRUE 1
+#define FALSE 0
+
 typedef int mensagens_t, *mensagens_pt;
 typedef int id_t;
 
-typedef struct pombo_t{
+typedef struct pombo{
 	mensagens_pt mensagens; // variável compartilhada entre os sites A e B
 	int limite;
+	sem_t* semaphore;
 }pombo_t, *pombo_pt;
 
-pombo_pt create_pombo(int limite_mensagens, mensagens_pt m)
+pombo_pt create_pombo(int limite_mensagens, mensagens_pt m, sem_t* sp)
 {
 	pombo_pt p = malloc(sizeof(pombo_pt));
 	p->limite = limite_mensagens;
 	p->mensagens = m;
-	return NULL;
+	p->semaphore = sp;
+	return p;
 }
 
-typedef struct pkg_site_t{
+/*
+typedef struct{
 	id_t id;
 	pombo_pt pombo;
-}pkg_site_t, *pkg_site_pt, pkg_site_A_t, pkg_site_B_t, *pkg_site_A_pt, *pkg_site_B_pt;
+}pkg_site_t, *pkg_site_pt;
 
-pkg_site_pt create_pkg_site(id_t id, pombo_pt p)
+typedef pkg_site_t pkg_site_A_t, pkg_site_B_t;
+typedef pkg_site_pt pkg_site_A_pt, pkg_site_B_pt;
+*/
+
+typedef struct{
+	id_t id;
+	pombo_pt pombo;
+}pkg_site_A_t, *pkg_site_A_pt;
+
+typedef struct {
+	id_t id;
+	pombo_pt pombo;
+}pkg_site_B_t, *pkg_site_B_pt;
+
+pkg_site_A_pt create_pkg_site_A(id_t id, pombo_pt p)
 {
-	pkg_site_pt site = malloc(sizeof(pkg_site_t));
+	pkg_site_A_pt site = malloc(sizeof(pkg_site_A_t));
+	if (!site)
+	{
+		printf("create_pkg_site_A: DEU ERRO!!!\n");
+		return NULL;
+	}
 	site->id = id;
 	site->pombo = p;
 	return site;
 }
 
-pkg_site_A_pt create_pkg_site_A(id_t id, pombo_pt p)
-{
-	return create_pkg_site(id, p);
-}
-
 void *site_A(void *args)
 {
-	printf("Thread A foi criada\n");
-	printf("Thread A será encerrada\n");
+	//printf("Thread A foi criada\n");
+	pkg_site_A_pt pkg_a = (pkg_site_A_pt)args;
+	pombo_pt pombo = pkg_a->pombo;
+	int i;
+	while (TRUE)
+	{
+		sem_wait(pombo->semaphore);
+		for (i = 1; i <= pombo->limite; i++)
+		{
+			pombo->mensagens++;
+			printf("Site A: escrevendo mensagem %d\n", i);
+		}
+		//envia mensagens
+		sem_post(pombo->semaphore);
+	}
+	//printf("Thread A serah encerrada\n");
 	return NULL;
 }
 
 pkg_site_B_pt create_pkg_site_B(id_t id, pombo_pt p)
 {
-	return create_pkg_site(id, p);
+	pkg_site_B_pt site = malloc(sizeof(pkg_site_B_t));
+	if (!site)
+	{
+		printf("create_pkg_site_B: DEU ERRO!!!\n");
+		return NULL;
+	}
+	site->id = id;
+	site->pombo = p;
+	return site;
 }
 
 void* site_B(void* args)
 {
-	printf("Thread B foi criada\n");
-	printf("Thread B será encerrada\n");
+	pkg_site_B_pt pkg_b = (pkg_site_B_pt)args;
+	pombo_pt pombo = pkg_b->pombo;
+	while (TRUE)
+	{
+		// aguardar receber mensagens
+		sem_wait(pombo->semaphore);
+		// assim que receber imprime quais mensagens recebeu
+		/*
+		loop até quantidade de mensagens
+			imprime "mensagem x recebida"
+		*/
+		printf("Site B: recebeu %d mensagen(s)\n", *(pombo->mensagens));
+		*(pombo->mensagens) = 0;
+		sem_post(pombo->semaphore);
+	}
+	/*printf("Thread B foi criada\n");
+	printf("Thread B serah encerrada\n");*/
 	return NULL;
 }
 
 int main()
 {
+	sem_t sem_pombo;
+	sem_init(&sem_pombo, 0, 1);
 	mensagens_t mensagens = 0;
-	pombo_pt pombo = create_pombo(20, &mensagens);
+	pombo_pt pombo = create_pombo(20, &mensagens, &sem_pombo);
 
 	id_t id_A = 0, id_B = 1;
 
 	pkg_site_A_pt pkg_A = create_pkg_site_A(id_A, pombo);
 	pkg_site_B_pt pkg_B = create_pkg_site_B(id_B, pombo);
+
+	if (!pkg_A || !pkg_B)
+	{
+		printf("Saindo do progrma...\n");
+		return -1;
+	}
 
 	pthread_t tsite_A, tsite_B;
 
